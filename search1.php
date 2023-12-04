f6ce039efc7345b0257faded5962956ec5adbadb
mediumsitompul <44793858+mediumsitompul@users.noreply.github.com>
2023-12-04 21:06:55 +0700

    Create search1.php

---
+++ search1.php
@@ -0,0 +1,40 @@
+<?php
+define('HOST', '192.168.100.240:8087');
+define('USER', 'pqr');
+define('PASS', 'Pensi2021');
+define('DB', 'db_restapi');
+$connect = mysqli_connect(HOST, USER, PASS, DB) or die('Not Connect');
+?>
+
+
+<?php
+
+$student_id = trim($_POST['search1']);
+
+$queryResult=$connect->
+
+query("SELECT id, 
+student_id, 
+student_name, 
+gender, 
+address, 
+major, 
+phone, 
+datetime_entry 
+FROM t_students 
+WHERE student_id='".$student_id."'");
+
+
+$result=array();
+
+while($fetchData=$queryResult->fetch_assoc()) {
+	$result[]=$fetchData;
+}
+
+    //Send back data to Flutter
+    if($result){
+        echo json_encode($result);
+    }else{
+        echo json_encode('');
+    }
+?>
